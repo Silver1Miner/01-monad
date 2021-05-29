f8d0caa6818014c4d6805872ef0fe5291d21b729
Silver <silver1miner@gmail.com>
2021-05-29 10:58:10 -0700

    randomize, drag controls

--- src/world.gd
+++ src/world.gd
@@ -8,6 +8,7 @@ onready var hud_control = $HUD
 onready var toggle_button = $HUD/Control/control_buttons/toggle_time
 onready var reset_button = $HUD/Control/control_buttons/reset
 onready var next_button = $HUD/Control/control_buttons/next_level
+onready var randomize_button = $HUD/Control/control_buttons/randomize
 onready var generation_display = $HUD/Control/left_pane/Generation
 onready var moves_display = $HUD/Control/left_pane/moves_display
 onready var par_display = $HUD/Control/left_pane/par_display
@@ -52,6 +53,8 @@ func _ready() -> void:
 		push_error("import button connect fail")
 	if export_button.connect("pressed", self, "_on_export_pressed") != OK:
 		push_error("export button connect fail")
+	if randomize_button.connect("pressed", self, "_on_random_pressed") != OK:
+		push_error("randomize button connect fail")
 	cell_size.x = TILE_SIZE
 	cell_size.y = TILE_SIZE
 	define_level(PlayerData.current_level)
@@ -66,6 +69,7 @@ func define_level(level):
 		if "dialogue" in Levels.levels[level]:
 			textbox.initialize(Levels.levels[level]["dialogue"])
 	elif level < 0: # Challenge
+		randomize_button.visible = false
 		populate_grid(target_state, Levels.challenges[PlayerData.current_challenge]["target"])
 		populate_grid(initial_state, Levels.challenges[PlayerData.current_challenge]["initial"])
 		par = Levels.challenges[PlayerData.current_challenge]["par"]
@@ -81,10 +85,12 @@ func define_level(level):
 		title.text = ""
 	if level > 0: # Campaign
 		has_target = true
+		randomize_button.visible = false
 		#moves_limited = true
 		hud_control.set_target_mode()
 		minimap.update()
 	if level > Levels.levels.keys().max():
+		randomize_button.visible = true
 		title.text = "Game Over"
 		print("campaign completed")
 		textbox.initialize(Levels.campaign_complete)
@@ -143,22 +149,33 @@ func _on_export_pressed() -> void:
 	input_state.text = data
 	OS.set_clipboard(data)
 
+var rng = RandomNumberGenerator.new()
+func _on_random_pressed() -> void:
+	for x in len(state):
+		for y in len(state[x]):
+			rng.randomize()
+			var random_number = rng.randi_range(0, 1)
+			state[x][y] = random_number
+			set_cell(x, y, random_number)
+
+var drag_enabled = false
+var pos = Vector2(0,0)
 func _input(event) -> void:
 	if event.is_action_pressed("ui_cancel"): # debugging
 		print("current level: ", PlayerData.current_level)
 		print("completed levels: ", PlayerData.completed_levels)
 		print("completed challenges: ", PlayerData.completed_challenges)
 	if event.is_action_pressed("left_mouse"): # changing tiles
-		var pos = (get_local_mouse_position()/TILE_SIZE).floor()
+		drag_enabled = true
+		pos = (get_local_mouse_position()/TILE_SIZE).floor()
 		if pos.x >= 0 and pos.x < w and pos.y >= 0 and pos.y < h and moves_left > 0:
 			state[pos.x][pos.y] = 1 - get_cellv(pos)
 			set_cellv(pos, 1 - get_cellv(pos))
 			$click.play()
 			moves += 1
 			moves_display.text = "Moves: " + str(moves)
-			#if moves_limited:
-			#	moves_left -= 1
-			#	par_display.text = "Par: " + str(par)
+	if event.is_action_released("left_mouse"):
+		drag_enabled = false
 
 func _on_play_toggled(toggled) -> void:
 	active = toggled
@@ -246,6 +263,15 @@ func _next_level() -> void:
 
 var accumulated = 0
 func _process(delta):
+	if drag_enabled:
+		var new_pos = (get_local_mouse_position()/TILE_SIZE).floor()
+		if new_pos != pos and new_pos.x >= 0 and new_pos.x < w and new_pos.y >= 0 and new_pos.y < h and moves_left > 0:
+			pos = new_pos
+			state[pos.x][pos.y] = 1 - get_cellv(pos)
+			set_cellv(pos, 1 - get_cellv(pos))
+			$click.play()
+			moves += 1
+			moves_display.text = "Moves: " + str(moves)
 	if active:
 		accumulated += delta
 		if accumulated > timestep:
